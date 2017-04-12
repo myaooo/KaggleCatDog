045e23d27210aee93ffb27ef601c3eacc9949a09
Ming Yao <yaoming.thu@gmail.com>
2017-04-12 14:54:57 -0400

    Fix bugs

--- cnn/model.py
+++ cnn/model.py
@@ -1,22 +1,25 @@
 from cnn.convnet.convnet import ConvNet
 from cnn.convnet.recorder import ConvRecorder
+from cnn.convnet.utils import init_tf_environ
 from cnn.preprocess import IMG_SIZE, CHANNELS, NUM_LABELS, prep_data
 
 num_epochs = 10
 BATCH_SIZE = 50
 EVAL_FREQUENCY = 1
 
+print('I love you. #heart#')
+
 
 def build_model():
 
     all_data = prep_data(test=False)
-    train_data = all_data[0][:1000]
-    train_labels = all_data[1][:1000]
+    train_data = all_data[0]
+    train_labels = all_data[1]
     valid_data = all_data[2]
     valid_labels = all_data[3]
     test_data = all_data[4]
     # LeNet-5 like Model
-    model = ConvNet('mnist')
+    model = ConvNet('lenet')
     model.push_input_layer(dshape=[None, IMG_SIZE[0], IMG_SIZE[1], CHANNELS])
     model.push_conv_layer(filter_size=[5, 5], out_channels=32, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
@@ -33,9 +36,15 @@ def build_model():
     model.set_optimizer('Adam')
     model.set_data(train_data, train_labels, valid_data, valid_labels)
     model.compile()
-    rec = ConvRecorder(model, '../model/minist/train')
-    model.train(BATCH_SIZE, 50, EVAL_FREQUENCY)
+    rec = ConvRecorder(model, '../models/lenet/train')
+    return model
 
+
+def main():
+    init_tf_environ(gpu_num=1)
+    model = build_model()
+    model.train(BATCH_SIZE, 1, EVAL_FREQUENCY)
+    model.save()
 
 if __name__ == '__main__':
-    build_model()
+    main()
