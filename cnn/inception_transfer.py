c7b82d159356c41fa131d09cd744819ce6bf4340
Ming Yao <yaoming.thu@gmail.com>
2017-04-13 10:51:11 -0400

    Update model hyperparameters

--- cnn/inception_transfer.py
+++ cnn/inception_transfer.py
@@ -2,18 +2,22 @@ import tensorflow as tf
 import cnn.inception.inception_train as inception_train
 # import cnn.inception.inception_eval as inception_eval
 from cnn.inception.dataset import CatDogData
-from cnn.convnet.utils import get_path
+from cnn.convnet.utils import get_path, init_tf_environ
 
 FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
+    init_tf_environ(1)
     FLAGS.data_dir = get_path('data/preprocessed/new')
     FLAGS.train_dir = get_path('models/inception_transfer')
     FLAGS.pretrained_model_checkpoint_path = get_path('models/inception-v3/model.ckpt-157585')
-    FLAGS.max_steps = 100
+    FLAGS.max_steps = 20000
     FLAGS.fine_tune = True
     FLAGS.subset = 'train'
+    FLAGS.initial_learning_rate = 0.001
+    FLAGS.num_epochs_per_decay = 5
+    FLAGS.learning_rate_decay_factor = 0.9
     dataset = CatDogData(subset=FLAGS.subset)
     assert dataset.data_files()
     if tf.gfile.Exists(FLAGS.train_dir):
