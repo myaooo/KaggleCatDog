4c9ab8a57311c5350c24273b06d8cdb58fda7b1e
mingyao <mingyao@191host020.cs.ust.hk>
2017-04-16 08:04:56 +0800

    Update dropout

--- cnn/model.py
+++ cnn/model.py
@@ -12,7 +12,7 @@ print('I love you. #heart#')
 
 def build_model(train_data, train_labels, valid_data, valid_labels):
     # LeNet-5 like Model
-    model = model2()
+    model = model3()
     model.set_data(train_data, train_labels, valid_data, valid_labels)
     model.compile()
     return model
@@ -40,16 +40,20 @@ def model1():
 def model2():
     model = ConvNet('lenet2')
     model.push_input_layer(dshape=[None, IMG_SIZE[0], IMG_SIZE[1], CHANNELS])
-    model.push_conv_layer(filter_size=[5, 5], out_channels=128, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[7, 7], out_channels=128, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
-    model.push_conv_layer(filter_size=[6, 6], out_channels=128, strides=[1, 1], activation='relu')
+    model.push_dropout_layer(0.7)
+    model.push_conv_layer(filter_size=[7, 7], out_channels=128, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
-    model.push_dropout_layer(0.5)
+    model.push_dropout_layer(0.7)
+    model.push_conv_layer(filter_size=[7, 7], out_channels=128, strides=[1, 1], activation='relu')
+    model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
+    model.push_dropout_layer(0.7)
     model.push_conv_layer(filter_size=[7, 7], out_channels=128, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
     model.push_flatten_layer()
     model.push_fully_connected_layer(out_channels=128, activation='relu')
-    model.push_dropout_layer(0.5)
+    model.push_dropout_layer(0.7)
     model.push_fully_connected_layer(out_channels=NUM_LABELS, activation='linear')
 
     model.set_loss('sparse_softmax')
@@ -74,7 +78,7 @@ def model3():
     model.push_dropout_layer(0.5)
     model.push_conv_layer(filter_size=[4, 4], out_channels=192, strides=[1, 1], activation='relu')
     model.push_conv_layer(filter_size=[1, 1], out_channels=192, strides=[1, 1], activation='relu')
-    model.push_conv_layer(filter_size=[1, 1], out_channels=NUM_LABELS, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[1, 1], out_channels=192, strides=[1, 1], activation='relu')
     model.push_pool_layer('avg', kernel_size=[1, int(IMG_SIZE[0]/4), int(IMG_SIZE[1]/4), 1], strides=[int(IMG_SIZE[0]/4), int(IMG_SIZE[1]/4)])
     model.push_flatten_layer()
     model.push_fully_connected_layer(out_channels=128,activation='relu')
@@ -83,7 +87,7 @@ def model3():
 
     model.set_loss('sparse_softmax')
     # model.set_regularizer('l2', 6e-5)
-    model.set_learning_rate(0.002)
+    model.set_learning_rate(0.0002)
     model.set_optimizer('Adam')
     return model
 
@@ -92,7 +96,7 @@ def main():
     all_data = prep_data(test=False)
     model = build_model(*all_data[:4])
     rec = ConvRecorder(model, get_path('models', 'lenet/train'))
-    model.train(BATCH_SIZE, 20, EVAL_FREQUENCY)
+    model.train(BATCH_SIZE, 30, EVAL_FREQUENCY)
     model.save()
 
 
