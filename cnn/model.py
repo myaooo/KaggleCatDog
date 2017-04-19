04281d8349ff25bb49c58e73951dfc8350cf0c0d
mingyao <mingyao@191host020.cs.ust.hk>
2017-04-20 04:41:54 +0800

    Change model

--- cnn/model.py
+++ cnn/model.py
@@ -3,7 +3,7 @@ from cnn.convnet.recorder import ConvRecorder
 from cnn.convnet.utils import init_tf_environ, get_path
 from cnn.data.preprocess import IMG_SIZE, CHANNELS, NUM_LABELS, prep_data, BATCH_SIZE
 
-num_epochs = 25
+num_epochs = 35
 EVAL_FREQUENCY = 1
 
 print('I love you. #heart#')
@@ -43,15 +43,15 @@ def model2():
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
     model.push_dropout_layer(0.5)
     model.push_conv_layer(filter_size=[5, 5], out_channels=96, strides=[1, 1], activation='relu')
-    model.push_conv_layer(filter_size=[3, 3], out_channels=96, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[5, 5], out_channels=96, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
     model.push_dropout_layer(0.5)
-    model.push_conv_layer(filter_size=[3, 3], out_channels=128, strides=[1, 1], activation='relu')
-    model.push_conv_layer(filter_size=[3, 3], out_channels=128, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[5, 5], out_channels=128, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[5, 5], out_channels=128, strides=[1, 1], activation='relu')
     model.push_pool_layer('max', kernel_size=[1, 2, 2, 1], strides=[2, 2])
     model.push_dropout_layer(0.5)
-    model.push_conv_layer(filter_size=[3, 3], out_channels=192, strides=[1, 1], activation='relu')
-    model.push_conv_layer(filter_size=[3, 3], out_channels=192, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[5, 5], out_channels=192, strides=[1, 1], activation='relu')
+    model.push_conv_layer(filter_size=[5, 5], out_channels=192, strides=[1, 1], activation='relu')
     model.push_pool_layer('avg', kernel_size=[1, int(IMG_SIZE[0]/8), int(IMG_SIZE[1]/8), 1], strides=[int(IMG_SIZE[0]/8), int(IMG_SIZE[1]/8)])
     model.push_flatten_layer()
     model.push_fully_connected_layer(out_channels=256, activation='relu')
@@ -59,7 +59,7 @@ def model2():
     model.push_fully_connected_layer(out_channels=NUM_LABELS, activation='linear')
     model.set_loss('sparse_softmax')
     # model.set_regularizer('l2', 1e-5)
-    model.set_learning_rate(0.002)
+    model.set_learning_rate(0.001)
     model.set_optimizer('Adam')
     return model
 
