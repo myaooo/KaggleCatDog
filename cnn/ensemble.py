466580b79258c828520b83b69d7584a30d69e72c
MYaooo <yaoming.thu@gmail.com>
2017-05-04 00:12:19 +0800

    fix ensemble

--- cnn/ensemble.py
+++ cnn/ensemble.py
@@ -34,7 +34,7 @@ def ensemble_predict(data_generator, models, weights=None):
         predictions = np.stack(predictions)
         prediction = np.mean(predictions, axis=0)
     else:
-	weights = weights / np.sum(weights)
+        weights = weights / np.sum(weights)
         for i in range(len(models)):
             predictions[i] = predictions[i] * weights[i]
         predictions = np.stack(predictions)
